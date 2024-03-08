a9db32870f807edfed96c3654f976dc87c013951
heidless <rob.lockhart@yahoo.co.uk>
2024-03-08 18:46:11 +0000

    creating routes & links - complete

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,63 @@
+import { useState, useContext } from 'react'
+import RatingSelect from "./RatingSelect"
+import Card from './shared/Card'
+import Button from './shared/Button'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  const {addFeedback} = useContext(FeedbackContext)
+
+  const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if ( text && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage("Text must contain at least 10 characters...")
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
+    setText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+
+      addFeedback(newFeedback)
+
+      setText('')
+    }
+  }
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your services with us?</h2>  
+        <RatingSelect select={(rating) => setRating(rating)}/>
+        <div className='input-group'>
+          <input 
+            onChange={handleTextChange} 
+            type='text' 
+            placeholder='Write a review...'
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
